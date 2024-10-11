588a709220ea3dd29fbcdd53a5829fcbd9eab6e1
Екатерина Богаченко <katyabo@list.ru>
2024-10-11 08:10:03 +0300

    Add validation for class Games, create extension for HTTP-methods, make Program.cs file more clean

--- GameStore.Api/Entities/Game.cs
+++ GameStore.Api/Entities/Game.cs
@@ -1,11 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Api.Entities;
 
 public class Game
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Название не должно быть пустым")]
+    [StringLength(100, ErrorMessage = "Название должно содержать от 1 до 100 символов")]
     public required string Name { get; set; }
+
+    [Required(ErrorMessage = "Жанр не должен быть пустым")]
+    [StringLength(50)]
     public required string Genre { get; set; }
+
+    [Range(0, 150, ErrorMessage = "Цена должна быть в диапазоне от 0 до 150 $")]
     public decimal Price { get; set; }
+
     public DateTime ReleaseDate { get; set; }
+
+    [Url]
+    [StringLength(100)]
     public required string ImageUri { get; set; }
 }
